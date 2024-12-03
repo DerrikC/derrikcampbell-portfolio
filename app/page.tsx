40ae948fbbbabab6f7e7b785fda31249d173eb2e
Derrik Campbell <derrikcampbell@Derriks-Laptop.local>
2024-12-03 16:41:00 -0500

    missed open character

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,13 @@
-import { Link } from "@nextui-org/link";
 import ProjectFilter from "@/components/project-filter";
-import { button as buttonStyles } from "@nextui-org/theme";
 import CompanyLogos from "@/components/company-logos";
-import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="max-w-4xl text-left">
         {/* this is causing issues with theme */}
-        <span className={title()}>Hi I&apos;m &nbsp;</span>
+        <span className={title()}>Hi I&apos;m&nbsp;</span>
         <span className={title({ color: "green" })}>Derrik Campbell</span>
         <br />
         <span className={title()}>
